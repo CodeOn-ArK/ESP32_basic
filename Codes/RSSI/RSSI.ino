5ccc521df2c1e06d19482dc00f8d9f72936cfd73
CodeOn-ArK <aarya.codeeka@gmail.com>
2021-05-26 19:55:52 +0530

    WiFi RSSI measurement code added

---
+++ Codes/RSSI/RSSI.ino
@@ -0,0 +1,40 @@
+#include <WiFi.h>
+
+const char* ssid     = "AndroidAPE9CC";
+const char* password = "AndroidAPE9C";
+
+void setup()
+{ 
+  Serial.begin(115200);     // set the LED pin mode
+
+    delay(10);
+
+    // We start by connecting to a WiFi network
+
+    Serial.println();
+    Serial.println();
+    Serial.print("Connecting to ");
+    Serial.println(ssid);
+
+    WiFi.begin(ssid, password);
+
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(500);
+        Serial.print(".");
+    }
+
+    Serial.println("");
+    Serial.println("WiFi connected.");
+    Serial.println("IP address: ");
+    Serial.println(WiFi.localIP());
+    
+}
+
+void loop () {
+  
+  // print the received signal strength:
+  long rssi = WiFi.RSSI();
+  Serial.print("RSSI:");
+  Serial.println(rssi);
+  delay(500);
+  }
--- Codes/Wifi_Web_Server/Wifi_Web_Server.ino
+++ Codes/Wifi_Web_Server/Wifi_Web_Server.ino
@@ -1,7 +1,7 @@
 #include <WiFi.h>
 
 const char* ssid     = "AndroidAPE9CC";
-const char* password = "AndroidAPE9C";
+const char* password = "AndroidAPE9CC";
 
 WiFiServer server(80);
 
